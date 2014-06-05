c9f59815e21ae52f27d0993437230f646198656f
Mike Kuzin <mkuzin@iKuz-R.local>
2014-06-05 15:28:30 -0400

    Adding og image

--- Gruntfile.js
+++ Gruntfile.js
@@ -314,7 +314,8 @@ module.exports = function (grunt) {
                         'images/{,*/}*.webp',
                         '{,*/}*.html',
                         'styles/fonts/{,*/}*.*',
-                        'bower_components/bootstrap-sass-official/vendor/assets/fonts/bootstrap/*.*'
+                        'bower_components/bootstrap-sass-official/vendor/assets/fonts/bootstrap/*.*',
+                        'opengraph/*.*'
                     ]
                 }]
             },
